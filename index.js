0e044c8408484d1505d4a0fc1e19a821c462f855
Marina <lutientinuviel99@gmail.com>
2022-06-09 14:27:45 +1000

    Примеры scss

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const today = new Date();
 window.console.log(today);
-window.console.log(today.toLocaleDateString());
-window.console.log(moment().format('DD.mm.yyyy'));
+const stringDate = today.toLocaleDateString();
+window.console.log(stringDate);
+const tomorrow = moment()
+    .add(1, 'days')
+    .format('DD-MM-yyyy');
+const dateBlock = document.getElementById('date');
+dateBlock.append(stringDate);
+dateBlock.append(`, Завтра: ${tomorrow}`);
+
