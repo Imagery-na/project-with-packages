ff304858a621a45cd4c9b2d378f0c9585406c2a7
Marina <lutientinuviel99@gmail.com>
2022-06-09 14:33:31 +1000

    empty

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const today = new Date();
 window.console.log(today);
 const stringDate = today.toLocaleDateString();
 window.console.log(stringDate);
-const tomorrow = moment()
-    .add(1, 'days')
-    .format('DD-MM-yyyy');
 const dateBlock = document.getElementById('date');
 dateBlock.append(stringDate);
-dateBlock.append(`, Завтра: ${tomorrow}`);
 
